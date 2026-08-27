e82eb3c0a34225dc7a2fa475b1ed33178f1e1b22
Dub26 <153666042+Dub26@users.noreply.github.com>
2026-08-27 16:12:24 +0300

    Models te eksik bilgi güncellenmesi

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from database import Base
 
 class Firma(Base):
-    # MySQL'deki tablonun ismini buraya tam eşleşecek şekilde yazıyoruz
     __tablename__ = "izto_firmalar"
 
-    id = Column(Integer, primary_key=True, index=True)
-    oda_sicil_no = Column(String, index=True)
+    # id sildik, oda_sicil_no primary_key oldu!
+    oda_sicil_no = Column(String, primary_key=True, index=True)
     ticari_sicil_no = Column(String, index=True)
-    unvani = Column(String, index=True)
     meslek_grubu = Column(String, index=True)
-    ilce = Column(String, index=True)
     nace_kodu = Column(String, index=True)
+    unvani = Column(String, index=True)
+    ilce = Column(String, index=True)
     tescilli_adresi = Column(String)
+    web_adresi = Column(String)
